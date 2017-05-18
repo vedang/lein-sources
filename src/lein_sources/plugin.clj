779f9d153f7c596eaddece01dacea3a959ca2c95
Vedang Manerikar <ved.manerikar@gmail.com>
2017-05-18 22:14:51 +0530

    Merge deps calculated by lein-sources into project

--- src/lein_sources/plugin.clj
+++ src/lein_sources/plugin.clj
@@ -2,7 +2,8 @@
   "A leiningen plugin to add sources and javadoc jars to the classpath."
   (:require [cemerick.pomegranate.aether :as aether]
             [clojure.pprint :refer [pprint]]
-            [leiningen.core.main :as lcm])
+            [leiningen.core.main :as lcm]
+            [leiningen.core.project :as p])
   (:import org.sonatype.aether.resolution.DependencyResolutionException))
 
 (defn- find-transitive-dependencies
@@ -51,13 +52,24 @@
           []
           deps))
 
+(defn- add-deps-to-project
+  [project source-dependencies]
+  (if (seq source-dependencies)
+    (let [lein-sources-profile {:dependencies source-dependencies}]
+      (p/merge-profiles project [lein-sources-profile]))
+    (do (lcm/info "No source dependencies to add!")
+        project)))
+
 (defn middleware
   [{:keys [repositories dependencies lein-sources-opts] :as project}]
+  (lcm/info "Inside lein-sources!")
   (let [reps (into aether/maven-central repositories)
         transitive-dependencies (find-transitive-dependencies dependencies reps)
         source-dependencies (resolve-source-dependencies transitive-dependencies
                                                          reps
                                                          lein-sources-opts)]
     (lcm/info "Adding the following dependencies to :lein-sources profile:\n"
-              (pprint source-dependencies))
-    project))
+              (with-out-str (pprint source-dependencies)))
+    (let [p (add-deps-to-project project source-dependencies)]
+      (lcm/info "Final Project Map: \n"
+                (with-out-str (pprint p))))))
